6b2dd83b7d6f85490985257a437eb5f231509e51
Sandro Almeida <sandro001@gmail.com>
2020-01-28 16:16:07 -0300

    Minifying styles.css file

--- projeto-webpack/client/webpack.config.js
+++ projeto-webpack/client/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');   //https://nodejs.org/api/path.html
 const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
+const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 let plugins = [];
 
@@ -10,6 +11,16 @@ plugins.push(
 
 if (process.env.NODE_ENV == 'production') {
     plugins.push(new babiliPlugin());
+
+    plugins.push(new optimizeCSSAssetsPlugin({
+        cssProcessor: require('cssnano'),
+        cssProcessorOptions: { 
+            discardComments: {
+                removeAll: true 
+            }
+        },
+        canPrint: true
+    }));
 }
 
 module.exports = {
